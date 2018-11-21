9f4abfd1f9e3b8c1c712bd41b39ec0c0618e77b3
wxuns <php7ml@gmail.com>
2018-11-21 22:15:48 +0800

    自动加载

---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: wxuns <wxuns@wxuns.cn>
+ * Link: http://wxuns.cn
+ * Date: 2018/11/21
+ * Time: 21:22
+ */
+function Commands()
+{
+    return [
+
+    ];
+}
